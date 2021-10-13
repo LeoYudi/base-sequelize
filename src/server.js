def33f4a54c67b2ceab72f4cab1535e63c8b30d6
Leonardo Yudi Higuti <leonardo.higuti@unesp.br>
2021-10-13 10:16:11 -0300

    feat: return errors

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('express-async-errors');
 require('dotenv').config();
 
 require('./database');
@@ -13,6 +14,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 app.use(routes);
+
+app.use((err, req, res, next) => {
+  if (err instanceof Error)
+    return res.status(400).json({ error: err.message });
+
+  console.log(err);
+  return res.status(500).json({ error: 'internal server error' });
+});
 
 const port = process.env.PORT || 3333;
 app.listen(port, () => console.log(`server running on port ${port}`));
